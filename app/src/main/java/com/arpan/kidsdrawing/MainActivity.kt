7327ea19eb9b5406b0b9ed4750054d4a3c9e3190
arpan-jo <arpanjorng@gmail.com>
2023-08-29 20:53:38 +0600

    image set from gallery

--- app/src/main/java/com/arpan/kidsdrawing/MainActivity.kt
+++ app/src/main/java/com/arpan/kidsdrawing/MainActivity.kt
@@ -1,26 +1,88 @@
 package com.arpan.kidsdrawing
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
-
-
     private var ibBrush: ImageButton? = null
+    private var imageSelect: ImageButton? = null
+
+    private val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK && result.data != null) {
+                val imageBackground: ImageView = findViewById(R.id.iv_background)
+                imageBackground.setImageURI(result.data?.data)
+            }
+        }
+
+    private val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+                if (isGranted) {
+                    Toast.makeText(this@MainActivity, "Permission granted", Toast.LENGTH_LONG)
+                        .show()
+
+
+                } else {
+                    val pickIntent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
+
+
+//                    if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+//                        Toast.makeText(
+//                            this@MainActivity,
+//                            "Permission not granted",
+//                            Toast.LENGTH_LONG
+//                        ).show()
+//                    }
+                }
+//                if (isGranted) {
+//                    permissionHandle(permissionName, true)
+//                } else {
+//                    permissionHandle(permissionName, false)
+//                }
+            }
+        }
+
+    private fun permissionHandle(permissionName: String, isGranted: Boolean) {
+        when (permissionName) {
+            Manifest.permission.READ_EXTERNAL_STORAGE -> {
+                Toast.makeText(
+                    this,
+                    if (isGranted) "Permission is granted." else "Permission is not granted.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+        imageSelect = findViewById(R.id.image_select)
+
 
         val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.paint_colors)
         mImageButtonCurrentPaint = linearLayoutPaintColors[2] as ImageButton
@@ -33,7 +95,15 @@ class MainActivity : AppCompatActivity() {
         ibBrush?.setOnClickListener {
             showBrushSizerDialog()
         }
+
+        val ibUndo: ImageButton = findViewById(R.id.image_undo)
+        ibUndo.setOnClickListener { }
+
+        imageSelect?.setOnClickListener {
+            requestStoragePermission()
+        }
     }
+
 
     private fun showBrushSizerDialog() {
         val brushDialog = Dialog(this)
@@ -73,5 +143,34 @@ class MainActivity : AppCompatActivity() {
             )
             mImageButtonCurrentPaint = view
         }
+    }
+
+
+    private fun requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            showRationalDialog(
+                "Kids Drawing app",
+                "Kids Drawing app needs to access your external storage"
+            )
+        } else {
+            requestPermission.launch(
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            )
+        }
+    }
+
+    private fun showRationalDialog(title: String, message: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title).setMessage(message).setPositiveButton("Cancel") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.create().show()
     }
 }
